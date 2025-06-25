e3b43f6955042f633ab247588205a98e6cd56549
cavin7777 <caven7777@gmail.com>
2025-06-25 14:00:58 +0000

    my commit

---
+++ functions_nested_loops/1-alphabet.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * print_alphabet - prints the alphabet in lowercase followed by a new line
+ *
+ * Description: Uses a buffer and _putchar only twice
+ */
+void print_alphabet(void)
+{
+    char alphabet[] = "abcdefghijklmnopqrstuvwxyz\n";
+    int i = 0;
+
+    while (alphabet[i] != '\0')
+    {
+        _putchar(alphabet[i]);
+        i++;
+    }
+}
