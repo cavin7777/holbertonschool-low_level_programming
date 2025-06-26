1273c3e54b665040b8713fdf6b9aebab8b06cc79
cavin7777 <caven7777@gmail.com>
2025-06-26 09:26:24 +0000

    my commit

--- functions_nested_loops/main.h
+++ functions_nested_loops/main.h
@@ -1,7 +1,7 @@
 #ifndef MAIN_H
 #define MAIN_H
 
-int _isalpha(int c);
-int _putchar(char c);  /* Only include this if you're using _putchar elsewhere */
+int _putchar(char c);
+int print_sign(int n);
 
-#endif /* MAIN_H */
+#endif
