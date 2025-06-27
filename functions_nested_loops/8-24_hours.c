2b560d27356810dd93d91480d59e3639dee10fbd
cavin7777 <caven7777@gmail.com>
2025-06-27 06:08:31 +0000

    my commit

--- functions_nested_loops/8-24_hours.c
+++ functions_nested_loops/8-24_hours.c
@@ -12,9 +12,9 @@ int hour, minute;
 
 for (hour = 0; hour < 24; hour++)
 {
-for (minute = 0; minute < 60; minute++)
-{
-printf("%02d:%02d\n", hour, minute);
-}
+	for (minute = 0; minute < 60; minute++)
+	{
+	printf("%02d:%02d\n", hour, minute);
+	}
 }
 }
