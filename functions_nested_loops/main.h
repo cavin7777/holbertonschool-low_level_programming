d27cb3847fd4549485d22d804b8ac36dc39e0169
cavin7777 <caven7777@gmail.com>
2025-06-27 07:37:28 +0000

    my commit

--- functions_nested_loops/main.h
+++ functions_nested_loops/main.h
@@ -1,6 +1,6 @@
-#ifndef MAIN_H
-#define MAIN_H
-
-int add(int, int);
-
-#endif /* MAIN_H */
+int main(void)
+{
+    print_to_98(0);   // Will print from 0 to 98
+    print_to_98(111); // Will print from 111 to 98
+    return 0;
+}
