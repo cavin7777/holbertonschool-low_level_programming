005483557acbf919e14e4f6556f0c1ec2af663ce
cavin7777 <caven7777@gmail.com>
2025-06-27 06:18:41 +0000

    my commit

--- functions_nested_loops/8-24_hours.c
+++ functions_nested_loops/8-24_hours.c
@@ -1,20 +1,31 @@
 #include "main.h"
-#include <stdio.h>
+#include <unistd.h>
 
 /**
  * jack_bauer - prints every minute of the day of Jack Bauer
  *
- * Description: Prints time in HH:MM format from 00:00 to 23:59
+ * Description: Uses write to print every minute in the format HH:MM
+ * from 00:00 to 23:59.
  */
 void jack_bauer(void)
 {
-int hour, minute;
+	int hour, minute;
+	char time[6];
 
-for (hour = 0; hour < 24; hour++)
-{
-	for (minute = 0; minute < 60; minute++)
+	for (hour = 0; hour < 24; hour++)
 	{
-	printf("%02d:%02d\n", hour, minute);
+		for (minute = 0; minute < 60; minute++)
+		{
+			time[0] = '0' + hour / 10;
+			time[1] = '0' + hour % 10;
+			time[2] = ':';
+			time[3] = '0' + minute / 10;
+			time[4] = '0' + minute % 10;
+			time[5] = '\n';
+
+			/* Write time and newline */
+			write(1, time, 6);
+		}
 	}
 }
-}
+
