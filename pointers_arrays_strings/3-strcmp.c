6b981ad5dbd456344ad6dd228557d43a52536fe5
cavin7777 <caven7777@gmail.com>
2025-07-06 14:57:04 +0400

    y commit

---
+++ pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * _strcmp - Compares two strings.
+ * @s1: First string.
+ * @s2: Second string.
+ *
+ * Return: An integer less than, equal to, or greater than 0
+ *         if s1 is found to be less than, to match, or be
+ *         greater than s2 respectively.
+ */
+int _strcmp(char *s1, char *s2)
+{
+	while (*s1 && (*s1 == *s2))
+	{
+		s1++;
+		s2++;
+	}
+	return (*s1 - *s2);
+}
+
