92ae8a7d51a52ba30dacfbe40b1fa28c9e19ce4b
cavin7777 <caven7777@gmail.com>
2025-07-06 15:41:52 +0400

    y commit

--- pointers_arrays_strings/6-cap_string.c
+++ pointers_arrays_strings/6-cap_string.c
@@ -1,28 +1,27 @@
 #include "main.h"
 
 /**
- * cap_string - capitalizes all words of a string
- * @str: the string to capitalize
+ * cap_string - capitalizes all words of a string except the first word
+ *              which is left unchanged
+ * @str: string to modify
  *
- * Return: pointer to the changed string
+ * Return: pointer to the modified string
  */
 char *cap_string(char *str)
 {
 	int i = 0;
-	int capitalize_next = 1;
+	int capitalize_next = 0;
 
-	while (str[i] != '\0')
+	/* Skip the first word */
+	while (str[i] && !(str[i] == ' ' || str[i] == '\t' || str[i] == '\n' ||
+						str[i] == ',' || str[i] == ';' || str[i] == '.' ||
+		str[i] == '!' || str[i] == '?' || str[i] == '"' ||
+		str[i] == '(' || str[i] == ')' || str[i] == '{' ||
+		str[i] == '}'))
+		i++;
+
+	while (str[i])
 	{
-		if (capitalize_next && (str[i] >= 'a' && str[i] <= 'z'))
-		{
-			str[i] = str[i] - ('a' - 'A');
-			capitalize_next = 0;
-		}
-		else if (!capitalize_next && (str[i] >= 'A' && str[i] <= 'Z'))
-		{
-			str[i] = str[i] + ('a' - 'A');
-		}
-
 		if (str[i] == ' ' || str[i] == '\t' || str[i] == '\n' ||
 		    str[i] == ',' || str[i] == ';' || str[i] == '.' ||
 		    str[i] == '!' || str[i] == '?' || str[i] == '"' ||
@@ -30,6 +29,15 @@ char *cap_string(char *str)
 		    str[i] == '}')
 		{
 			capitalize_next = 1;
+		}
+		else if (capitalize_next && (str[i] >= 'a' && str[i] <= 'z'))
+		{
+			str[i] -= 'a' - 'A';
+			capitalize_next = 0;
+		}
+		else if (!capitalize_next && (str[i] >= 'A' && str[i] <= 'Z'))
+		{
+			str[i] += 'a' - 'A';
 		}
 		else
 		{
