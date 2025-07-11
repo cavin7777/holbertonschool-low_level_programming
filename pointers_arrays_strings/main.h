06d33899ed4b267a0166653fef86fbb24690b568
cavin7777 <caven7777@gmail.com>
2025-07-11 09:34:21 +0400

    y commit

--- pointers_arrays_strings/main.h
+++ pointers_arrays_strings/main.h
@@ -16,5 +16,6 @@ char *cap_string(char *str);
 char *leet(char *s);
 char *_memset(char *s, char b, unsigned int n);
 char *_memcpy(char *dest, char *src, unsigned int n);
+char *_strchr(char *s, char c);
 
 #endif /* MAIN_H */
