45f0140f346a6d763958e1f8d1fca8ed0c8ea94c
cavin7777 <caven7777@gmail.com>
2025-07-20 14:10:42 +0400

    y commit

---
+++ structures_typedef/dog.h
@@ -0,0 +1,17 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - structure for a dog
+ * @name: Name of the dog (string)
+ * @age: Age of the dog (float)
+ * @owner: Owner of the dog (string)
+ */
+struct dog
+{
+    char *name;
+    float age;
+    char *owner;
+};
+
+#endif /* DOG_H */
