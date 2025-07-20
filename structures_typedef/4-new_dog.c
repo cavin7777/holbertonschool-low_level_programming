f5ac819f5b1f263c46aff20d0a8a3a88274086b6
cavin7777 <caven7777@gmail.com>
2025-07-20 15:08:58 +0400

    y commit

--- structures_typedef/4-new_dog.c
+++ structures_typedef/4-new_dog.c
@@ -43,35 +43,35 @@ char *_strcpy(char *dest, char *src)
  */
 dog_t *new_dog(char *name, float age, char *owner)
 {
-    dog_t *dog;
-    int name_len, owner_len;
+	dog_t *dog;
+	int name_len, owner_len;
 
-    dog = malloc(sizeof(dog_t));
-    if (dog == NULL)
-        return (NULL);
+	dog = malloc(sizeof(dog_t));
+	if (dog == NULL)
+		return (NULL);
 
-    name_len = _strlen(name);
-    owner_len = _strlen(owner);
+	name_len = _strlen(name);
+	owner_len = _strlen(owner);
 
-    dog->name = malloc(sizeof(char) * (name_len + 1));
-    if (dog->name == NULL)
-    {
-        free(dog);
-        return (NULL);
-    }
+	dog->name = malloc(sizeof(char) * (name_len + 1));
+	if (dog->name == NULL)
+	{
+	free(dog);
+		return (NULL);
+	}
 
-    dog->owner = malloc(sizeof(char) * (owner_len + 1));
-    if (dog->owner == NULL)
-    {
-        free(dog->name);
-        free(dog);
-        return (NULL);
-    }
+	dog->owner = malloc(sizeof(char) * (owner_len + 1));
+	if (dog->owner == NULL)
+	{
+		free(dog->name);
+		free(dog);
+	return (NULL);
+	}
 
-    _strcpy(dog->name, name);
-    _strcpy(dog->owner, owner);
-    dog->age = age;
+	_strcpy(dog->name, name);
+	_strcpy(dog->owner, owner);
+	dog->age = age;
 
-    return (dog);
+	return (dog);
 }
 
