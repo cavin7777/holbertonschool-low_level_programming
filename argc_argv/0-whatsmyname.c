bcfd708e523f113aeeb166fa7c1737707f276423
cavin7777 <caven7777@gmail.com>
2025-08-07 13:43:03 +0400

    version 1

---
+++ argc_argv/0-whatsmyname.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - prints the name of the program
+ * @argc: argument to count
+ * @argv: array of argument strings
+ *
+ * Return: 0 (sucess)
+ */
+int main(int argc, char *argv[])
+{
+	(void)argc;
+	printf("%s\n", argv[0]);
+	return (0);
+}
