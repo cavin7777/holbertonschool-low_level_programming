e7d93796abf9acc593a1158ccf3cb2584529c75d
cavin7777 <caven7777@gmail.com>
2025-08-21 10:08:53 +0400

    betty style v2

--- more_malloc_free/1-string_nconcat.c
+++ more_malloc_free/1-string_nconcat.c
@@ -1,5 +1,4 @@
 #include <stdlib.h>
-#include <string.h>
 
 /**
  * string_nconcat - concatatane two strings
@@ -15,13 +14,9 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	char *concat;
 
 	if (s1 == NULL)
-	{
 		s1 = "";
-	}
 	if (s2 == NULL)
-	{
 		s2 = "";
-	}
 
 	while (s1[len1] != '\0')
 		len1++;
@@ -29,25 +24,15 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 		len2++;
 
 	if (n >= len2)
-	{
 		n = len2;
-	}
 
 	concat = malloc((len1 + n + 1) * sizeof(char));
 	if (concat == NULL)
-	{
 		return (NULL);
-	}
-
 	for (i = 0; i < len1; i++)
-	{
 		concat[i] = s1[i];
-	}
-
 	for (j = 0; j < n; j++)
-	{
 		concat[i + j] = s2[j];
-	}
 
 	concat[i + j] = '\0';
 
