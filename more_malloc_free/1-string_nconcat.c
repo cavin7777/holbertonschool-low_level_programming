6770548c9c0684b2cafae0700d303b8b588ebe4e
cavin7777 <caven7777@gmail.com>
2025-08-21 09:50:43 +0400

    concatenate

---
+++ more_malloc_free/1-string_nconcat.c
@@ -0,0 +1,53 @@
+#include <stdlib.h>
+#include <string.h>
+
+/**
+ * string_nconcat - concatatane two strings
+ * @s1: 1st string
+ * @s2: 2nd string
+ * @n: number to concatenate
+ *
+ * Return: the concatenation or NULL
+ */
+char *string_nconcat(char *s1, char *s2, unsigned int n)
+{
+	unsigned int i, j, len1 = 0, len2 = 0;
+	char *concat;
+
+	if (s1 == NULL)
+	{
+		s1 = "";
+	}
+	if (s2 == NULL)
+	{
+		s2 = "";
+	}
+
+	len1 = strlen(s1);
+	len2 = strlen(s2);
+
+	if (n >= len2)
+	{
+		n = len2;
+	}
+
+	concat = malloc((len1 + n + 1) * sizeof(char));
+	if (concat == NULL)
+	{
+		return (NULL);
+	}
+
+	for (i = 0; i < len1; i++)
+	{
+		concat[i] = s1[i];
+	}
+
+	for (j = 0; j < n; j++)
+	{
+		concat[i + j] = s2[j];
+	}
+
+	concat[i + j] = '\0';
+
+	return (concat);
+}
