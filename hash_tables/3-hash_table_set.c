51ee7742285ea5f909b02ffa08f75826082b7f6c
cavin7777 <caven7777@gmail.com>
2025-08-27 10:54:45 +0400

    correc.key

--- hash_tables/3-hash_table_set.c
+++ hash_tables/3-hash_table_set.c
@@ -34,9 +34,10 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 			free(current->value);
 			current->value = strdup(value);
 			if (current->value == NULL)
-			return (0);
-		}
+				return (0);
 		return (1);
+		}
+	current = current->next;
 	}
 
 	new_element->key = strdup(key);
