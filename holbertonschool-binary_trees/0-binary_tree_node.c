eb46b5c1ec897387734c15055a4d31f679343986
cavin7777 <caven7777@gmail.com>
2025-08-31 13:55:44 +0400

    node_only_gcc_ok

---
+++ holbertonschool-binary_trees/0-binary_tree_node.c
@@ -0,0 +1,28 @@
+#include <stdlib.h>
+#include "binary_trees.h"
+
+/**
+ * binary_tree_node - Creates a binary tree node
+ * @parent: Pointer to the parent node
+ * @value: Value to store in the new node
+ *
+ * Return: Pointer to the new node or NULL
+ */
+binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
+{
+	binary_tree_t *new_node;
+
+	/* Always start malloc and NULL value */
+	new_node = malloc(sizeof(binary_tree_t));
+	if (new_node == NULL)
+		return (NULL);
+
+    /* Then the new node */
+	new_node->n = value;
+	new_node->parent = parent;
+	/* left and right are NULL for the moment */
+	new_node->left = NULL;
+	new_node->right = NULL;
+
+	return (new_node);
+}
